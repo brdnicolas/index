899f925eacda4f322bf0a50c97614f5a332acef9
Nicolas Brouard <brdnicolas.contact@gmail.com>
2024-08-13 17:42:43 +0200

    fix: build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { AboutMe } from '@/pages/landing/sections/aboutMe/AboutMe'
-import { AwardsList } from '@/pages/landing/sections/awardsList/AwardsList'
-import { Introduction } from '@/pages/landing/sections/introduction/Introduction'
-import { Marquee } from '@/pages/landing/sections/marquee/Marquee'
-import { Presentation } from '@/pages/landing/sections/presentation/Presentation'
-import { Projects } from '@/pages/landing/sections/projects/Projects'
+import { AboutMe } from '@/containers/landing/sections/aboutMe/AboutMe'
+import { AwardsList } from '@/containers/landing/sections/awardsList/AwardsList'
+import { Introduction } from '@/containers/landing/sections/introduction/Introduction'
+import { Marquee } from '@/containers/landing/sections/marquee/Marquee'
+import { Presentation } from '@/containers/landing/sections/presentation/Presentation'
+import { Projects } from '@/containers/landing/sections/projects/Projects'
 
 export default function Home() {
   return (
